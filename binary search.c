fa973011cdc4623bbafdb58d4972441457dd1d85
SharonSurya46 <sharonsurya2405@gmail.com>
2024-12-05 09:34:13 +0530

    Update binary search.c

--- binary search.c
+++ binary search.c
@@ -1,133 +1,27 @@
-#include<stdio.h>
-int main()
-{
-	int a[10]={1,2,3,4,5,6,7,8,9,10};
-	int target=9;
-	int low=0;
-	int high=9;
-	int mid;
-	while(low<=high)
-	{
-		mid=low+(high-low)/2;
-		if(a[mid]==target)
-		{
-			printf("%d",mid);
-			return 0;
-		}
-		if(a[mid]<target)
-		{
-			low=mid+1;
-		}
-		else
-		{
-			high=mid-1;
-		}
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		\\
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-	}
-	printf("not found");
-	return 0;
+#include <stdio.h>
+
+int binarySearch(int arr[], int size, int key) {
+    int low = 0, high = size - 1, mid;
+    while (low <= high) {
+        mid = (low + high) / 2;
+        if (arr[mid] == key)
+            return mid;
+        else if (arr[mid] < key)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+    return -1;
+}
+
+int main() {
+    int arr[] = {10, 20, 30, 40, 50};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int key = 30;
+    int result = binarySearch(arr, size, key);
+    if (result != -1)
+        printf("Element found at index %d\n", result);
+    else
+        printf("Element not found\n");
+    return 0;
 }
